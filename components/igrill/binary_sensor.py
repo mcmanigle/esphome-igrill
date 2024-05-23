768dcd66f0f0518bf403637fa675585c1c14c607
John McManigle <john@oxfordechoes.com>
2024-05-22 21:04:42 -0400

    get variable?

--- components/igrill/binary_sensor.py
+++ components/igrill/binary_sensor.py
@@ -46,7 +46,7 @@ CONFIG_SCHEMA = cv.All(
 
 
 async def to_code(config):
-    var = cg.Pvariable(config[CONF_ID])
+    var = await cg.get_variable(config[CONF_ID])
 
     if CONF_TEMPERATURE_PROBE1_PLUGGED in config:
         sens = await binary_sensor.new_binary_sensor(config[CONF_TEMPERATURE_PROBE1_PLUGGED])
